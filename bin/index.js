c1e2dfbd454f9480fec5f8ad0d4acb61b62fe341
Atikah <Atikah@aia.com>
2020-09-20 02:35:40 +0700

    chore(main): add yargs and use store

--- bin/index.js
+++ bin/index.js
@@ -3,4 +3,18 @@
 // Set options as a parameter, environment variable, or rc file.
 // eslint-disable-next-line no-global-assign
 require = require("esm")(module/* , options */)
-module.exports = require("../src/main.js").cli(process.argv)
+const yargs = require('yargs');
+
+function parseArguments() {
+  const commands = yargs
+    .command('get', 'Show exisiting task list')
+    .command('done <task-id>', 'Set specific task to done')
+    .command('refresh', 'Get newest data from server')
+    .command('upload', 'Send local update to server')
+    .demandCommand()
+    .help()
+    .argv;
+  return commands;
+}
+
+module.exports = require("../src/main.js").cli(parseArguments())
--- src/main.js
+++ src/main.js
@@ -1,12 +1,60 @@
-// ESM syntax is supported.
+import TaskStore from './store/TaskStore';
+
+async function commandResolver(argument) {
+
+  const { _: command, taskId} = argument;
+  
+  if (!TaskStore.isInitialized) {
+    TaskStore.setName("TaskList");
+    await TaskStore.initialize();
+  }
+
+  switch (command[0]) {
+    case 'get':
+      // console.log('getting data..')
+      const taskList = TaskStore.data.map(item => {
+        return item
+      })
+      return taskList;
+      // break;
+    case 'done':
+      // console.log(`setting ${taskId} to done..`);
+      await TaskStore.editItem(`${taskId}`, { status: 'done' })
+        .then(() => {
+          return `Task ${taskId} status is set to done!`;
+        }).catch(err => {
+          throw new Error(err.message);
+        })
+      break;
+    case 'refresh': 
+      // console.log(`refreshing data..`);
+      await TaskStore.watchRemote().then(() => {
+        return `Task data is updated`;
+      });
+      break;
+    case 'upload':
+      // console.log(`uploading data..`);
+      await TaskStore.upload().then(() => {
+          return `Local tasks is uploaded!`;
+        }).catch(err => {
+          throw new Error(err.message);
+        })
+      break;
+    default:
+      throw new Error('Command not available, please check your input');
+  }
+}
+
 /**
  * 
  * @param {*} args
- * [] show existing task list
- * [] complete a task
- * [] get & send update
- * [] could work offline 
  */
-export function cli(args){
-  console.log(args)
+export async function cli(args){
+
+  try {
+    await commandResolver(args);
+  } catch (e) {
+    console.log(e);
+  }
+
 }
