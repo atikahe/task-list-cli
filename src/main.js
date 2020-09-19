21c2a421da8262d73f00dff7c611704333a41e0c
Atikah <Atikah@aia.com>
2020-09-19 23:26:40 +0700

    (init): setting up env and structure

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// ESM syntax is supported.
+/**
+ * 
+ * @param {*} args
+ * [] show existing task list
+ * [] complete a task
+ * [] get & send update
+ * [] could work offline 
+ */
+export function cli(args){
+  console.log(args)
+}
